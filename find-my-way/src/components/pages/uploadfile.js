d29b20fad9609a84e14065139d344b59091f60c4
Matt Lawlor <mattlawlor@mattmacbook-pro.student.iastate.edu>
2018-03-04 14:59:56 -0600

    changed upload file

--- find-my-way/src/components/pages/uploadfile.js
+++ find-my-way/src/components/pages/uploadfile.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
-import ImagesUploader from 'react-images-uploader';
-import 'react-images-uploader/styles.css';
-import 'react-images-uploader/font.css';
+import Dropzone from 'react-dropzone'
+// import ImagesUploader from 'react-images-uploader';
+// import 'react-images-uploader/styles.css';
+// import 'react-images-uploader/font.css';
 
-class UploadFile extends Component {
+// class UploadFile extends Component {
+//     render() {
+//         return (
+//             <ImagesUploader
+//                 url="http://localhost:9090/notmultiple"
+//                 optimisticPreviews
+//                 multiple={false}
+//                 onLoadEnd={(err) => {
+//                     if (err) {
+//                         console.error(err);
+//                     }
+//                 }}
+//                 label="Upload a picture"
+//             />
+//         );
+//     }
+// }
+
+// export default UploadFile;
+
+class UploadFile extends React.Component {
+    constructor() {
+        super()
+        this.state = { files: [] }
+    }
+
+    onDrop(files) {
+        this.setState({
+            files
+        });
+    }
+
     render() {
         return (
-            <ImagesUploader
-                url="http://localhost:9090/notmultiple"
-                optimisticPreviews
-                multiple={false}
-                onLoadEnd={(err) => {
-                    if (err) {
-                        console.error(err);
-                    }
-                }}
-                label="Upload a picture"
-            />
+            <section>
+                <div className="dropzone">
+                    <Dropzone onDrop={this.onDrop.bind(this)}>
+                        <p>Try dropping some files here, or click to select files to upload.</p>
+                    </Dropzone>
+                </div>
+                <aside>
+                    <h2>Dropped files</h2>
+                    <ul>
+                        {
+                            this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+                        }
+                    </ul>
+                </aside>
+            </section>
         );
     }
 }
