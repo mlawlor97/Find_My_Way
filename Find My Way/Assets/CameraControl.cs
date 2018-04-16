1876ff1039e386380195526d39f0b8cc3dcf1b25
Stair <jstair@iastate.edu>
2018-04-16 17:23:55 -0500

    Orientation support and touch controls

--- Find My Way/Assets/CameraControl.cs
+++ Find My Way/Assets/CameraControl.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
-	Vector3 reference;
+	GameObject cameraChild;
 	public float compassSpeed;
+	public float zoomSensitivity;
+	public float translateSensitivity;
+	public float rotationSensitivity;
+	bool useGyroControl;
+	float zoom;
+
+	public Vector3 dbg;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +22,10 @@ public class CameraControl : MonoBehaviour {
 		//Input.compass.enabled = true;
 		Input.location.Start();
 		Physics.autoSimulation = false;
+
+		useGyroControl = false;
+		cameraChild = transform.Find ("Main Camera").gameObject;
+		zoom = 1;
 	}
 	
 	// Update is called once per frame
@@ -22,18 +33,75 @@ public class CameraControl : MonoBehaviour {
 		if (!Input.compass.enabled) {
 			Input.compass.enabled = true;
 		}
-		transform.Rotate (-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
-		//transform.rotation = Quaternion.Euler (0, -Input.compass.trueHeading, 0);
-//		transform.rotation = Quaternion.Euler (Vector3.SmoothDamp (
-//			transform.rotation.eulerAngles,
-//			new Vector3 (transform.rotation.eulerAngles.x, -Input.compass.trueHeading, transform.rotation.eulerAngles.z),
-//			ref reference,
-//			compassSpeed * Time.deltaTime));
-		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
-			Mathf.LerpAngle(transform.rotation.eulerAngles.y, Input.compass.trueHeading, compassSpeed * Time.deltaTime),
-			0);
-		if (Input.GetTouch (0).position.x < Screen.width / 5) {
-			transform.rotation = Quaternion.Euler (0, 0, 0);
+		if (useGyroControl) {
+			//rotate with gyro
+			transform.rotation = Quaternion.Euler (90, 0, 0) *
+			new Quaternion (Input.gyro.attitude.x, Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+			transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles.x,
+												   Mathf.LerpAngle (transform.rotation.eulerAngles.y, Input.compass.trueHeading, compassSpeed * Time.deltaTime),
+												   0);
+		} else {
+			//rotate with gestures
+			if (Input.touchCount == 1) {
+				Touch touch = Input.GetTouch (0);
+				Vector2 touchDelta = touch.deltaPosition;
+				/*transform.Rotate(Vector3.up     * touchDelta.x * rotationSensitivity +
+								 -Vector3.right * touchDelta.y * rotationSensitivity);*/
+				transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles.x - touchDelta.y * rotationSensitivity,
+													   transform.rotation.eulerAngles.y + touchDelta.x * rotationSensitivity,
+													   0f);
+			}
 		}
+
+		//adjust zoom with orientation
+		if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight) {
+			cameraChild.transform.localPosition = new Vector3 (cameraChild.transform.localPosition.x,
+															   cameraChild.transform.localPosition.y,
+															   -25 * zoom);
+		} else {
+			cameraChild.transform.localPosition = new Vector3 (cameraChild.transform.localPosition.x,
+															   cameraChild.transform.localPosition.y,
+															   -25 * zoom * 2);
+		}
+
+		//zoom with gesture
+		if (Input.touchCount == 2) {
+			// Store both touches.
+			Touch touchZero = Input.GetTouch (0);
+			Touch touchOne = Input.GetTouch (1);
+
+			// Find the position in the previous frame of each touch.
+			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+			// Find the magnitude of the vector (the distance) between the touches in each frame.
+			float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+			// Find the difference in the distances between each frame.
+			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+			// Otherwise change the field of view based on the change in distance between the touches.
+			zoom += deltaMagnitudeDiff * zoomSensitivity;
+		}
+
+		//translate with gesture
+		if (Input.touchCount == 2) {
+			Touch touch0 = Input.GetTouch (0);
+			Touch touch1 = Input.GetTouch (1);
+			Vector2 touchDeltaAvg = (touch0.deltaPosition + touch1.deltaPosition) / 2;
+			Vector3 weirdForward = new Vector3 (transform.forward.x, 0f, transform.forward.z).normalized;
+			Vector3 weirdRight = new Vector3 (transform.right.x, 0f, transform.right.z).normalized;
+			transform.position += -weirdForward * touchDeltaAvg.y * translateSensitivity +
+				-weirdRight   * touchDeltaAvg.x * translateSensitivity;
+		}
+	}
+
+	public void toggleGyroControl () {
+		useGyroControl = !useGyroControl;
+	}
+
+	public void setZoomSensitivity (float newZoomSensitivity) {
+		zoomSensitivity = newZoomSensitivity;
 	}
 }
---
+++ Find My Way/Assets/GenerateSchematics.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GenerateSchematics : MonoBehaviour {
+
+	public bool readBinary;
+	public float wallHeight;
+	public float wallWidth;
+	public TextAsset ta;
+	public TextAsset bf;
+	public GameObject wall;
+	public GameObject positioner;
+	GameObject wallCopy;
+	GameObject positionerCopy;
+
+	// Use this for initialization
+	void Start () {
+		string schematic = ta.text;
+		int i = 0;
+		if (!readBinary) {
+			while (i < schematic.Length) {
+				if (schematic [i] == '\n') {
+					i++;
+				} else if (schematic [i] == ' ') {
+					i++;
+				} else if (schematic [i] == 'b') {
+					int j = i;
+					while (schematic [j] != '\n') {
+						j++;
+					}
+					readBox (schematic.Substring (i, j - i));
+					i += j - i;
+				} else if (schematic [i] == 'w') {
+					int j = i;
+					while (schematic [j] != '\n') {
+						j++;
+					}
+					readWall (schematic.Substring (i, j - i));
+					i += j - i;
+				} else {
+					Debug.Log ("error reading text file");
+					i++;
+				}
+			}
+		} else {
+
+		}
+	}
+	
+	void readBox(string line){
+		Debug.Log (line);
+		int i = 2;
+		float posX, posY, width, height, rot;
+		posX = parseNextInt (line, ref i);
+		posY = parseNextInt (line, ref i);
+		width = parseNextInt (line, ref i);
+		height = parseNextInt (line, ref i);
+		rot = parseNextInt (line, ref i);
+//		Debug.Log ("posX: " + posX);
+//		Debug.Log ("posY: " + posY);
+//		Debug.Log ("width: " + width);
+//		Debug.Log ("height: " + height);
+//		Debug.Log ("rot: " + rot);
+		positionerCopy = Instantiate (positioner, new Vector3 (posX, 0, posY), Quaternion.identity);
+		wallCopy = Instantiate (wall, new Vector3 (posX, 0, posY + height / 2), Quaternion.identity, positionerCopy.transform);
+		wallCopy.transform.localScale = new Vector3 (width, wallHeight, wallWidth);
+		wallCopy = Instantiate (wall, new Vector3 (posX, 0, posY - height / 2), Quaternion.identity, positionerCopy.transform);
+		wallCopy.transform.localScale = new Vector3 (width, wallHeight, wallWidth);
+		wallCopy = Instantiate (wall, new Vector3 (posX + width / 2, 0, posY), Quaternion.identity, positionerCopy.transform);
+		wallCopy.transform.localScale = new Vector3 (wallWidth, wallHeight, height);
+		wallCopy = Instantiate (wall, new Vector3 (posX - width / 2, 0, posY), Quaternion.identity, positionerCopy.transform);
+		wallCopy.transform.localScale = new Vector3 (wallWidth, wallHeight, height);
+		positionerCopy.transform.rotation = Quaternion.Euler (new Vector3 (0, rot, 0));
+	}
+
+	void readWall(string line){
+		Debug.Log (line);
+		int i = 2;
+		float pos1X, pos1Y, pos2X, pos2Y;
+		pos1X = parseNextInt (line, ref i);
+		pos1Y = parseNextInt (line, ref i);
+		pos2X = parseNextInt (line, ref i);
+		pos2Y = parseNextInt (line, ref i);
+//		Debug.Log ("pos1X: " + pos1X);
+//		Debug.Log ("pos1Y: " + pos1Y);
+//		Debug.Log ("pos2X: " + pos2X);
+//		Debug.Log ("pos2Y: " + pos2Y);
+		wallCopy = Instantiate (wall, new Vector3 ((pos1X + pos2X) / 2, 0, (pos1Y + pos2Y) / 2), Quaternion.identity);
+		wallCopy.transform.localScale = new Vector3 (wallWidth, wallHeight, Vector3.Distance(new Vector3(pos1X, 0, pos1Y), new Vector3(pos2X, 0, pos2Y)));
+		wallCopy.transform.LookAt (new Vector3 (pos2X, 0, pos2Y));
+	}
+
+	float parseNextInt (string line, ref int i){
+		int j = i;
+		while (line [j] != ' ') {
+			j++;
+			if (j >= line.Length) {
+				break;
+			}
+		}
+		float result = float.Parse (line.Substring (i, j - i));
+		i = j + 1;
+		return result;
+	}
+}
