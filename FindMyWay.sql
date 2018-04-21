ad8ab035ef3a59e2928b90f91e8aea1b1d0c7ee7
Mitchell Kerr <mrkerr@iastate.edu>
2018-04-21 17:33:38 -0500

    New user and image tables

--- FindMyWay.sql
+++ FindMyWay.sql
@@ -1,7 +1,23 @@
-Create table Users(
-username varchar(255),
-password varchar(255));Users
+CREATE TABLE `users` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `first_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `last_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `created` datetime NOT NULL,
+ `modified` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-insert into Users values('matty', 'password');
 
-select * from Users;
+CREATE TABLE Images(
+userid int(5) not null,
+topLeftLat FLoat(6,4) not null,
+topLeftLong FLoat(6,4) not null,
+topRightLat FLoat(6,4) not null,
+topRightLong FLoat(6,4) not null,
+rotation int(5),
+dataURL MediumText not null,
+uniqueName varchar(100) not null,
+PRIMARY KEY (uniqueName)
+);
