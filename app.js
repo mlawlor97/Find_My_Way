5afe9eede5b8134554bc2976267223a40444f66b
root <root@findmyway.ece.iastate.edu>
2018-04-23 14:27:31 -0500

    boy

--- app.js
+++ app.js
@@ -1,70 +1,35 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var express = require("express");
+var login = require('./routes/loginroutes');
+var upload = require('./routes/fileroutes');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-
+/*
+Module:multer
+multer is middleware used to handle multipart form data
+*/
+var multer = require('multer');
+var multerupload = multer({ dest: 'fileprint/' })
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Content-Range, Content-Disposition, Content-Description, Accept");
+    res.header('Access-Control-Allow-Methods: DELETE, HEAD, GET, OPTIONS, POST, PUT');
+    next();
+});
+var router = express.Router();
 
-    //intercepts OPTIONS method
-    if ('OPTIONS' === req.method) {
-        //respond with 200
-        res.send(200);
-    }
-    else {
-        //move on
-        next();
-    }
+// test route
+router.get('/', function (req, res) {
+    res.json({ message: 'welcome to our upload module apis' });
 });
 
-var cors = require('cors');
-
-// use it before all route definitions
-app.use(cors({origin: '*'}));
-
-app.use('/', index);
-app.use('/users', users);
-
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
+//route to handle user registration
+router.post('/register', login.register);
+router.post('/login', login.login);
+//route to handle file printing and listing
+router.post('/fileupload', multerupload.any(), upload.fileupload);
+app.use('/api', router);
+app.listen(4000);
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
-
-router.post('/api/login', function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    var email = "mlawlor@iastate.edu";
-    var password = "password";
-
-    if(req.body.password === password && req.body.email === email){
-      res.json({message: "you are logged in"});
-    } else {
-        res.status(204).json({id: 204, error:err.message});
-    }
-
-});
-
 module.exports = router;
