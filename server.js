9cf4ff15eef27c91c4565e6763bfa7caf1bf370b
Matt Lawlor <mattlawlor@mattmacbook-pro.student.iastate.edu>
2018-04-27 12:47:18 -0500

    added stuff

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const port = process.env.PORT || 5050;
 
 var floorplans = {images: []};
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'FeyenDMaiWey!1969',
+  database: 'findmyway'
+});
+connection.connect(function (err) {
+  if (!err) {
+    console.log("Database is connected ... nn");
+  } else {
+    console.log("Error connecting database ... nn");
+  }
+});
+
 app.post('/api/maps', (req, res) => {
   console.log(req.route);
   var car = {type: 'a good one', model: 'T', color: 'red'};
