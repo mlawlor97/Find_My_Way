d0a93c4cd5bd9f036df00b5e0d0ec54c13dfbecb
Matt Lawlor <mattlawlor@mattmacbook-pro.student.iastate.edu>
2018-04-27 12:48:54 -0500

    should work

--- server.js
+++ server.js
@@ -38,4 +38,68 @@ app.post('/api/floorplans', (req, res) => {
   console.log(req);
 });
 
+app.post('/api/register', (req, res) => {
+  var today = new Date();
+  var users = {
+    "first_name": req.body.first_name,
+    "last_name": req.body.last_name,
+    "email": req.body.email,
+    "password": req.body.password,
+    "created": today,
+    "modified": today
+  }
+  connection.query('INSERT INTO users SET ?', users, function (error, results, fields) {
+    if (error) {
+      console.log("error ocurred", error);
+      res.send({
+        "code": 400,
+        "failed": "error ocurred"
+      })
+    } else {
+      console.log('The solution is: ', results);
+      res.send({
+        "code": 200,
+        "success": "user registered sucessfully"
+      });
+    }
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+  connection.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
+    if (error) {
+      // console.log("error ocurred",error);
+      res.send({
+        "code": 400,
+        "failed": "error ocurred"
+      })
+    } else {
+      // console.log('The solution is: ', results);
+      if (results.length > 0) {
+        if ([0].password == password) {
+          res.send({
+            "code": 200,
+            "success": "login sucessfull"
+          });
+        }
+        else {
+          res.send({
+            "code": 204,
+            "success": "Email and password does not match"
+          });
+        }
+      }
+      else {
+        res.send({
+          "code": 204,
+          "success": "Email does not exits"
+        });
+      }
+    }
+  });
+
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
